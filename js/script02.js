a6817cf154274ce5e3ff628db65c52a057155a9c
Isaac Daniel <isaacinfo2017@gmail.com>
2021-01-02 12:33:19 -0300

    Updates

--- js/script02.js
+++ js/script02.js
@@ -1,3 +1,8 @@
+let anoAtual = document.getElementById("ano") 
+let data = new Date() 
+let ano = data.getFullYear()
+anoAtual.innerHTML = `${ano}`
+
 function esconder(){
     let painel = document.getElementsByClassName("panel-body")[0]
     painel.style.display = "none"
