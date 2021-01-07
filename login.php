0ea1a21571fe3511558c059b12d8fcdbddecbf2e
Isaac Daniel <isaacinfo2017@gmail.com>
2021-01-07 12:30:06 -0300

    Updates

--- login.php
+++ login.php
@@ -2,18 +2,18 @@
    include('banco.php');
 
    //recebendo as variáveis por post
-   $usuario = $_POST['nome'];
+   $nome = $_POST['nome'];
    $email = $_POST['email'];
    $telefone = $_POST['telefone'];
  
    //criando uma consulta mysql
-   $sql  = "select nome, email, telefone from usuario where nome='$usuario' and email='$email' and telefone='$telefone'";
+   $sql  = "select nome, email, telefone from usuario where nome='$nome' and email='$email' and telefone='$telefone'";
  
    $consulta = $conexao->query($sql);
    
    if($consulta->num_rows > 0) {
-      $objeto_usuario = $consulta->fetch_array(MYSQLI_ASSOC);
-	   if($objeto_usuario['nome'] == "Julyan" && $objeto_usuario['telefone'] == "(88) 9 9873-4533"){
+      $usuario = $consulta->fetch_array(MYSQLI_ASSOC);
+	   if($usuario['nome'] == "Julyan" && $usuario['telefone'] == "(88) 9 9873-4533"){
        //inicializar a sessão
 	      session_start();
 	      $_SESSION['login']='ok';
@@ -22,8 +22,8 @@
         //inicializar a sessão
 	      session_start();
 	      $_SESSION['login'] = 'ok';
-	      $_SESSION['nome'] = $usuario;
-         $_SESSION['email'] = $email;
+	      //$_SESSION['nome'] = $nome;
+         //$_SESSION['email'] = $email;
          $_SESSION['telefone'] = $telefone;
          header('Location: usuario/procedimentos.php?login=ok');
       }  
