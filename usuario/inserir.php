522f24fafc770b3eaee01a63030feaef39a5204b
Isaac Daniel <isaacinfo2017@gmail.com>
2021-01-08 14:06:08 -0300

    Update inserir.php

--- usuario/inserir.php
+++ usuario/inserir.php
@@ -1,9 +1,4 @@
-<?php
-	session_start();
-	if(!isset($_SESSION['login'])){
-		header('Location: ../index.php?login=semsessao');
-    }
-    
+<?php    
     include('../banco.php');
 
     $nome = $_POST['nome']; 
