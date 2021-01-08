fb0b27b4b04c47e907f8631b726925fc38b79cce
Isaac Daniel <isaacinfo2017@gmail.com>
2021-01-08 15:39:40 -0300

    Update ponto_de_venda.sql

--- banco-de-dados/ponto_de_venda.sql
+++ banco-de-dados/ponto_de_venda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 08-Jan-2021 às 19:35
+-- Tempo de geração: 08-Jan-2021 às 19:39
 -- Versão do servidor: 10.4.17-MariaDB
 -- versão do PHP: 8.0.0
 
